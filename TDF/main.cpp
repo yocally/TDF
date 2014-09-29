fe9a6196f82cb16ccf589d6002d354c9fd5d48d5
Cally Sibben <themodempress@gmail.com>
2014-09-28 19:11:27 -0500

    Update

--- TDF/main.cpp
+++ TDF/main.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string>
 #include <SDL.h>
@@ -8,62 +7,73 @@
 
 bool init();
 void loadMedia();
-SDL_Surface* loadSurface(std::string path);
+SDL_Texture* loadTexture(std::string path);
+void close();
 void refresh();
+
+
+// Globals
+
+SDL_Window* gWindow = NULL;
+SDL_Renderer* gRenderer = NULL;
+SDL_Texture* gBackground = NULL;
+SDL_Texture* gChar = NULL;
+
 
 // Classes
 
-class Knight {
+class Knight{
 	int xLocal;
 	int yLocal;
 
 public:
-	SDL_Surface* surf = loadSurface("rec/char.png");
+	SDL_Texture* tex = gChar;
+
+
+	void setTex(SDL_Texture* loadedTex) {
+		tex = loadedTex;
+	}
+
 	void setRect(int x, int y) {
 		xLocal = x;
 		yLocal = y;
 	}
+
 	SDL_Rect getRect() {
 		SDL_Rect re;
 		re.x = xLocal;
 		re.y = yLocal;
 		return re;
 	}
+
 };
 
 
-// Globals
+// Class Global Def
 
-int SCREEN_WIDTH = 640;
-int SCREEN_HEIGHT = 480;
-SDL_Window* gWindow = NULL;
-SDL_Surface* gScreenSurface = NULL;
-SDL_Surface* gBackground = NULL;
-bool quit = false;
-SDL_Event e;
-Knight mainKnight;
+Knight knight;
+
 
 // Functions
 
 bool init() {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not initialize!\nSDL_Error: %s\n", SDL_GetError());
+	gWindow = SDL_CreateWindow("The Discovery Forge", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (gWindow == NULL) {
+		printf("Window could not be created!\n SDL_Error: %s\n", SDL_GetError());
 		return false;
 	}
 	else {
-		gWindow = SDL_CreateWindow("The Discovery Forge", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL) {
-			printf("Failed to create window\nSDL_Error: %s\n", SDL_GetError());
+		gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+		if (gRenderer == NULL) {
+			printf("Renderer could not be created!\n SDL_Error: %s\n", SDL_GetError());
 			return false;
 		}
 		else {
+			SDL_SetRenderDrawColor(gRenderer, 86, 165, 255, 255);
 			int imgFlags = IMG_INIT_PNG;
 			if (!(IMG_Init(imgFlags) & imgFlags)) {
-				printf("SDL_image could not initialize!\nSDL_image error: %s\n", IMG_GetError());
+				printf("SDL_image could not initialize! SDL_image error: %s\n", IMG_GetError());
 				return false;
-			}
-			else {
-				gScreenSurface = SDL_GetWindowSurface(gWindow);
 			}
 		}
 	}
@@ -71,48 +81,60 @@ bool init() {
 }
 
 void loadMedia() {
-	gBackground = loadSurface("rec/background.png");
+	gBackground = loadTexture("rec/background.png");
+	gChar = loadTexture("rec/background.png");
 }
 
-SDL_Surface* loadSurface(std::string path) {
-	// WARNING! Potential errors
-	SDL_Surface* optimizedSurface = NULL;
-	SDL_RWops* loadedSurface_rwops = SDL_RWFromFile(path.c_str(), "rb");
-	SDL_Surface* loadedSurface = IMG_LoadPNG_RW(loadedSurface_rwops);
+SDL_Texture* loadTexture(std::string path) {
+	SDL_Texture* newTexture = NULL;
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
 	if (loadedSurface == NULL) {
-		printf("Unable to load image %s\nSDL_image Error: %s\n", path.c_str(), IMG_GetError());
+		printf("Unable to load image %s! SDL_image Error: $s\n", path.c_str(), IMG_GetError());
 	}
 	else {
-		optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, NULL);
-		if (optimizedSurface == NULL) {
-			printf("Unable to optimize image %s\nSDL_image Error: %s\n", path.c_str(), IMG_GetError());
+		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		if (newTexture == NULL) {
+			printf("Unable to create texture from %s! SDL_Error: %s\n", path.c_str(), SDL_GetError());
 		}
 		SDL_FreeSurface(loadedSurface);
 	}
-	
-	return optimizedSurface;
+	return newTexture;
+}
+
+void close() {
+	SDL_DestroyTexture(gBackground);
+	gBackground = NULL;
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	gRenderer = NULL;
+	IMG_Quit();
+	SDL_Quit();
 }
 
 void refresh() {
-	SDL_BlitSurface(gBackground, NULL, gScreenSurface, NULL);
-	mainKnight.setRect(100, 100);
-	SDL_BlitSurface(mainKnight.surf, NULL, gScreenSurface, &mainKnight.getRect());
-	SDL_UpdateWindowSurface(gWindow);
+	SDL_RenderClear(gRenderer);
+	SDL_RenderCopy(gRenderer, gBackground, NULL, NULL);
+	SDL_RenderCopy(gRenderer, knight.tex, NULL, &knight.getRect());
+	SDL_RenderPresent(gRenderer);
 }
 
-int main(int argc, char* args[]) {
+int main(int argc, char* argv[]) {
 	if (!init()) {
 		printf("Init failed");
 	}
-	mainKnight.setRect(100, 100);
 	loadMedia();
-	refresh();
+	knight.setTex(gChar);
+	knight.setRect(100, 100);
+	bool quit = false;
+	SDL_Event e;
 	while (!quit) {
 		while (SDL_PollEvent(&e) != 0) {
 			if (e.type == SDL_QUIT) {
 				quit = true;
 			}
 		}
+		refresh();
 	}
 	return 0;
 }
